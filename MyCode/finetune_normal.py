37e4e20856db568e7c32321a5ea49aa842e0589e
Thetang-145 <nopphawan.nnsw@gmail.com>
2023-03-27 10:31:31 +0000

    run full text abstract

--- MyCode/finetune_normal.py
+++ MyCode/finetune_normal.py
@@ -12,16 +12,6 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 
 
-ENT_TYPES = ['Task', 'Method', 'Material', 'Metric', 'OtherScientificTerm', 'Generic']
-REL_TYPES = {
-    'FEATURE-OF': 'Asym', 
-    'PART-OF': 'Asym', 
-    'USED-FOR': 'Asym', 
-    'EVALUATE-FOR': 'Asym',
-    'HYPONYM-OF': 'Asym', 
-    'COMPARE': 'Sym',
-    'CONJUNCTION': 'Sym',
-}
 RAWDATAFILES = {
     "train": "training_complete.jsonl",
     "val": "validation_complete.jsonl",
@@ -42,83 +32,10 @@ def print_progress(curr, full, desc='', bar_size=30):
     sys.stdout.write(f"\r{desc}[{'='*bar}{' '*(bar_size-bar)}] {curr+1}/{full}")
     sys.stdout.flush()
     if curr+1==full: print()
-    
-
-def getInputDataset(data_split, section):
-    main_path = str((Path().absolute()).parents[0])
-    filepath = f"{main_path}/PL-Marker/_scire_models/{section}/{data_split}_re.json"
-    with open(filepath, 'r') as json_file:
-        json_list = list(json_file)
-    return [json.loads(json_str) for json_str in json_list]
-
-def addTripEnt(allEnt, tripEnt, ent):
-    try:
-        tripEnt[ent] = allEnt[ent]
-    except:
-        tripEnt[ent] = None
-    return tripEnt
-
-def getTripSeq(data, sym=True):
-    all_sentences = [j for i in data["sentences"] for j in i]
-    flatten_ner = [j for i in data["predicted_ner"] for j in i]
-    flatten_re  = [j for i in data["predicted_re"] for j in i[1]]
-    tripSeq = ""
-    tripEnt = {}
-    allEnt = {}
-
-    for ner in flatten_ner:
-        ent = " ".join(all_sentences[ner[0]:ner[1]+1])
-        allEnt[ent] = ner[2]
-
-    for rel in flatten_re:
-        s = " ".join(all_sentences[rel[0][0]:(rel[0][1]+1)])
-        o = " ".join(all_sentences[rel[1][0]:(rel[1][1]+1)])
-        p = rel[2]
-        tripSeq += f"{s} {p} {o}. "
-        if sym and REL_TYPES[p]=='Sym':
-            tripSeq += f"{o} {p} {s}. "
-        tripEnt = addTripEnt(allEnt, tripEnt, s)
-        tripEnt = addTripEnt(allEnt, tripEnt, o)
-        
-    freeEnt = allEnt.copy()
-    for key, val in tripEnt.items(): freeEnt.pop(key)
-
-    return tripEnt, freeEnt, tripSeq
-
-def getFreeEntSeq(freeEnt):
-    freeEntList = {}
-    for k, v in freeEnt.items():
-        if v not in list(freeEntList.keys()): 
-            freeEntList[v] = [k]
-        else:
-            freeEntList[v].append(k)
-    freeEntSeq = ""
-    for entType, ents in freeEntList.items():
-        if len(ents)==1:
-            freeEntSeq += f"{entType} is {ents[0]}. "
-        else:
-            freeEntSeq += f"{entType} are {', '.join(ents[:-1])}"
-            freeEntSeq += f", and {ents[-1]}. "
-    return freeEntSeq
-
-def getInputDF(data_split, section, prototype):
-    dataset = getInputDataset(data_split, section)
-    dataset_list = []
-    for i, data in enumerate(dataset):
-        tripEnt, freeEnt, tripSeq = getTripSeq(data)
-        # for entType in ENT_TYPES:
-        freeEntSeq = getFreeEntSeq(freeEnt)
-        row = {
-            "paper_id": data['doc_key'], 
-            "input_seq": tripSeq+freeEntSeq
-        }
-        dataset_list.append(row)
-        if isinstance(prototype, int):
-            if i>=prototype-1: break
-    return pd.DataFrame(dataset_list)
 
 
-def getTargetDF(data_split):
+
+def getDataset(data_split):
     main_path = str((Path().absolute()).parents[0])    
     filepath = f"{main_path}/MuP_sum/dataset/{RAWDATAFILES[data_split]}"
     with open(filepath, 'r') as json_file:
@@ -129,6 +46,7 @@ def getTargetDF(data_split):
         data = json.loads(json_str)
         dataset_list.append({
             "paper_id": data["paper_id"], 
+            "input_seq": data["paper"]["abstractText"], 
             "target_seq": data["summary"]
         })   
         print_progress(i, data_len, desc=f'Loading summary ({data_split})')
@@ -136,19 +54,16 @@ def getTargetDF(data_split):
     
     
 
-def prepro_data(data_split, section, prototype):
-    input_df = getInputDF(data_split, section, prototype)
-    target_df = getTargetDF(data_split)
-    merged_df = pd.merge(input_df, target_df, on='paper_id')
+def prepro_data(data_split):
+    dataset_df = getDataset(data_split)
     
     # remove paper with issue
     issue_doc = pd.read_csv("issue_data.csv")
     remove_index = []
     for paper_id in list(issue_doc['paper_id']):
-        remove_index += list(merged_df[merged_df['paper_id']==paper_id].index)
-    remove_index
-    merged_df_drop = merged_df.drop(index=remove_index)
-    return merged_df_drop
+        remove_index += list(dataset_df[dataset_df['paper_id']==paper_id].index)
+    dataset_df_drop = dataset_df.drop(index=remove_index)
+    return dataset_df_drop
 
 class MyDataset(Dataset):
     def __init__(self, data, tokenizer):
@@ -239,7 +154,6 @@ def main():
     parser.add_argument("--train", action='store_true', help="train model")
     parser.add_argument("--getSum", action='store_true', help="get generated summary from fine-tuned model")
     parser.add_argument("--eval", action='store_true', help="evaluate generated summary")
-    parser.add_argument('--prototype', type=int, help="number of data for prototype run")
     args = parser.parse_args()
     
             
@@ -247,16 +161,16 @@ def main():
     model = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
     
     if args.train:
-        train_data = prepro_data("train", section="abstract", prototype=args.prototype)
+        train_data = prepro_data("train")
         train(model, tokenizer, train_data)
     
     if args.getSum:
-        eval_data = prepro_data("val", section="abstract", prototype=args.prototype)
+        eval_data = prepro_data("val")
         result_df = generateSummary(model, tokenizer, eval_data)
-        result_df.to_csv("model/result_kg.csv")
+        result_df.to_csv("model/result_fullAbstract.csv")
         
     if args.eval:
-        
+        pass
         
 if __name__ == "__main__":
     main()
