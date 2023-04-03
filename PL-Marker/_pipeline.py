fcfffe857915fa9bb4db0b9b1dc9c10cd95431a2
Thetang-145 <nopphawan.nnsw@gmail.com>
2023-04-03 03:39:36 +0000

    add arXiv dataset and run some PL-marker for arXiv

--- PL-Marker/_pipeline.py
+++ PL-Marker/_pipeline.py
@@ -2,33 +2,35 @@ import os
 import argparse
 
 
-def getRE(section, data_split, gpu):
+def getRE(dataset, section, data_split, gpu):
     os.system(f"CUDA_VISIBLE_DEVICES={gpu} python3 _getRE.py  --model_type bertsub  \
     --model_name_or_path /pretrained_model/scire-scibert  --do_lower_case  \
-    --data_dir _sciner_models/{section}  \
+    --data_dir _sciner_models/{dataset}/{section}  \
     --learning_rate 2e-5  --num_train_epochs 10  --per_gpu_train_batch_size  8  --per_gpu_eval_batch_size 16  --gradient_accumulation_steps 1  \
     --max_seq_length 256  --max_pair_length 16  --save_steps 2500  \
     --do_eval  --eval_all_checkpoints  --eval_logsoftmax  \
     --fp16   \
     --test_file {data_split}_ner.json  \
     --use_ner_results \
-    --output_dir _scire_models/{section}  --output_file {data_split}_re.json")
+    --output_dir _scire_models/{dataset}/{section}  --output_file {data_split}_re.json")
         
-def getNER(section, data_split, gpu):
+def getNER(dataset, section, data_split, gpu):
     os.system(f"CUDA_VISIBLE_DEVICES={gpu} python3  _getNER.py  --model_type bertspanmarker  \
     --model_name_or_path  pretrained_model/sciner-scibert  --do_lower_case  \
-    --data_dir _prepared_data/{section}  \
+    --data_dir _prepared_data/{dataset}/{section}  \
     --learning_rate 2e-5  --num_train_epochs 50  --per_gpu_train_batch_size  8  --per_gpu_eval_batch_size 16  --gradient_accumulation_steps 1  \
     --max_seq_length 512  --save_steps 2000  --max_pair_length 256  --max_mention_ori_length 8    \
     --do_eval  --evaluate_during_training   --eval_all_checkpoints  \
     --fp16  --seed 42  --onedropout  --lminit  \
     --test_file {data_split}.jsonl  \
-    --output_dir _sciner_models/{section}  --output_file {data_split}_ner.json \
+    --output_dir _sciner_models/{dataset}/{section}  --output_file {data_split}_ner.json \
     --overwrite_output_dir  --output_results")
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--section", default='summary', type=str,
+    parser.add_argument("--dataset", default='MuP', type=str,
+                        help="which dataset to be operate: MuP, arXiv etc.")
+    parser.add_argument("--section", default='abstract', type=str,
                         help="which data to be operate: summary, abstract, section_1, section_introduction, etc.")
     
     parser.add_argument("--nopre", action='store_true', help="prepareMuP")
@@ -55,19 +57,31 @@ def main():
     if args.train: train = "--train "
     if args.val:   val   = "--val "
     if args.test:  test  = "--test "
-    
+                
     if not args.nopre:
-        command = f"python3 _prepareMuP.py --section {args.section} {train}{val}{test}"
-        utput = os.system(command)
-        
+        if args.dataset=="MuP":
+            command = f"python3 _prepareMuP.py --section {args.section} {train}{val}{test}"
+        elif args.dataset=="arXiv":
+            command = f"python3 _prepareArXiv.py --section {args.section} {train}{val}{test}"
+        output = os.system(command)
+            
     if not args.noner:
         for k, v in data_split.items():
-            if v: getNER(args.section, k, args.gpu)        
-        
+            if v: 
+                if k=="train" and args.dataset=="arXiv":
+                    getNER(args.dataset, args.section, f'{k}1', args.gpu)  
+                    getNER(args.dataset, args.section, f'{k}2', args.gpu)  
+                else:
+                    getNER(args.dataset, args.section, k, args.gpu) 
+
     if not args.norel:
         for k, v in data_split.items():
-            if v: getRE(args.section, k, args.gpu)    
-
-
+            if v:
+                if k=="train" and args.dataset=="arXiv":
+                    getRE(args.dataset, args.section, f'{k}1', args.gpu)  
+                    getRE(args.dataset, args.section, f'{k}2', args.gpu)  
+                else:
+                    getRE(args.dataset, args.section, k, args.gpu)
+                
 if __name__ == "__main__":
     main()
---
+++ PL-Marker/_prepareArXiv.py
@@ -0,0 +1,120 @@
+import os
+import sys
+import json
+from pathlib import Path
+import spacy
+import argparse
+
+def print_progress(curr, full, desc='', bar_size=50):    
+    bar = int((curr+1)/full*bar_size)
+    sys.stdout.write(f"\r{desc}[{'='*bar}{' '*(bar_size-bar)}] {curr+1}/{full}")
+    sys.stdout.flush()
+    
+def load_data(filepath, section):
+    with open(filepath, 'r') as json_file:
+        json_list = list(json_file)
+    dataset = []
+    data_len = len(json_list)
+    cant_load = 0
+    for i, json_str in enumerate(json_list):
+        try:
+            line = json.loads(json_str)
+            if section=="abstract":
+                dataset.append({
+                    "doc_key": line["article_id"],
+                    "sentences": (" ".join(line["abstract_text"])).replace("<S>", "").replace("</S>", ""),
+                })
+            elif section[:7]=="section":
+                sec_no = int(section[7:])-1
+                dataset.append({
+                    "doc_key": line["article_id"],
+                    "sentences": " ".join(line["sections"][sec_no]),
+                })
+        except: 
+            cant_load += 1
+        print_progress(i, data_len, desc=f'Loading {section} ')
+    print(f"\nSuccessfully import {data_len-cant_load}/{data_len} samples")
+    return dataset
+
+def convert_data(data):
+    num_docs = len(data)
+    nlp = spacy.load("en_core_web_sm")
+    conv_data = []
+    for idx, doc in enumerate(data):
+        conv_data.append({})
+        doc_text = nlp(doc["sentences"])
+        sents, ners, rels = [], [], []
+        for sent in doc_text.sents:
+            token_sent = [str(tok) for tok in sent]
+            sents.append(token_sent)
+            ners.append([])
+            rels.append([])
+        print_progress(idx, num_docs,  desc='Converting data ')
+        conv_data[idx]["doc_key"] = doc["doc_key"]
+        conv_data[idx]["sentences"] = sents
+        conv_data[idx]["ner"] = ners
+        conv_data[idx]["relations"] = rels
+    print()
+    return conv_data
+
+def export_data(data, output_dir, filename, append=False):
+    print("Writing prepared data to jsonl file")
+    mode = 'a+' if append else 'w'
+    print(output_dir)
+    if not(Path(output_dir).exists()): 
+        os.system(f"mkdir -p {output_dir}")
+        # os.mkdir(output_dir, 0755)
+    with open(output_dir+filename, mode, encoding='utf-8') as f:
+        for line in data:
+            json_record = json.dumps(line, ensure_ascii=False)
+            f.write(json_record + '\n')
+    print('Wrote {} records to {}'.format(len(data), output_dir+filename))
+    
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--section", default='abstract', type=str,
+                        help="which data to be prepated: abstract, section_1")
+    parser.add_argument("--output_dir", default='_prepared_data/arXiv', type=str,
+                        help="Directory of prepared data")
+    
+    parser.add_argument("--train", action='store_true', help="operate on train data")
+    parser.add_argument("--val", action='store_true', help="operate on validate data")
+    parser.add_argument("--test", action='store_true', help="operate on test data")
+
+    parser.add_argument("--split", action='store_true', help="split training set to smaller subfiles")
+    
+    args = parser.parse_args()
+    
+    if args.split:
+        output_dir = f"{args.output_dir}/{args.section}/"
+        
+        exit()
+
+    if not (args.train or args.val or args.test):
+        args.train = args.val = args.test = True
+        
+    runPrepare = {
+        "train": args.train,
+        "val": args.val,
+        "test": args.test,
+    }
+        
+    
+    main_path = str((Path().absolute()).parents[0])
+    dataset_path = main_path+"/dataset_arXiv/"
+    
+    
+    for key, val in runPrepare.items():
+        if val:
+            print(f"{'*'*25} Processing {key} data {'*'*25}")
+            data = load_data(f"{dataset_path}{key}.txt", args.section)
+            data = convert_data(data)
+            export_data(
+                data, 
+                output_dir = f"{args.output_dir}/{args.section}/", 
+                filename   = f"{key}.jsonl"
+            )
+        
+if __name__ == "__main__":
+    main()
